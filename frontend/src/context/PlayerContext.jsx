d360ccfb241ce4830ec853c490a90108a55e08ea
kauser1987 <128887629+kauser1987@users.noreply.github.com>
2024-07-07 11:15:44 +0530

    Update PlayerContext.jsx

--- frontend/src/context/PlayerContext.jsx
+++ frontend/src/context/PlayerContext.jsx
@@ -9,7 +9,7 @@ const PlayerContextProvider = (props)=>{
     const seekbg = useRef()
     const seekBar = useRef()
 
-    const url="http://localhost:4000";
+    const url="https://projectspotify-backend.onrender.com";
     const [songsData, setSongsData] = useState([])
     const [albumsData, setAlbumsData] = useState([])
     const [track,setTrack] = useState(songsData[0]);
